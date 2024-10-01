1d6bb04c34b94b27c0e219fc06b737c4fcc6448c
codeAKstan <boystanismoses@gmail.com>
2024-10-01 17:19:04 +0100

    Learninng route and synamic url handling

--- app.py
+++ app.py
@@ -3,8 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return "<h2>About Page</h2><p>This is the about page.</p>"
+
+@app.route('/contact')
+def contact():
+    return "<h2>Contact Page</h2><p>Contact us at contact@example.com</p>"
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f"<h2>User Profile</h2><p>Hello, {username}!</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
