318fdcd73bf768d8d9bcb454e02708506f0d3fd7
codeAKstan <boystanismoses@gmail.com>
2024-10-08 10:36:37 +0100

    control structure with jinja2

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html', title="Home page", user="codeAKstan")
 
+@app.route('/items')
+def items():
+    items = ['apple', 'banana', 'grape', 'strawberry']
+    return render_template('items.html', items=items)
 
 if __name__ == "__main__":
     app.run(debug=True)
