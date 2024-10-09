c475d39c13d7a97d033d35c448a0679825cb9a7c
codeAKstan <boystanismoses@gmail.com>
2024-10-09 14:19:55 +0100

    Handling get and post request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,18 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        user_name = request.form['name']
+        return f"Hello, {user_name}!"
+    else:
+        return "Please submit the form using POST."
+
 
 if __name__ == "__main__":
     app.run(debug=True)
