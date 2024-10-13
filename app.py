1dc93c38246deffed2e240a31a5a7f04c6f1006b
codeAKstan <boystanismoses@gmail.com>
2024-10-13 19:16:57 +0100

    connecting database

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
-from flask import Flask, request, render_template, url_for, redirect, flash
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TelField
-from wtforms.validators import DataRequired
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
-class NameForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(message="Please enter your email")])
-    number = TelField('Tele', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+db = SQLAlchemy(app)
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        flash(f"Hello, {form.name.data}!", 'success')
-        return redirect(url_for('index'))
-    return render_template('form_wtf.html', form=form)
+# Define a User Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
