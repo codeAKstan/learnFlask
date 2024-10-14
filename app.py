fa26c5bd7237759e0dc17300211cb68e5b1ef0eb
codeAKstan <boystanismoses@gmail.com>
2024-10-14 11:27:52 +0100

    diaplaying the users in the index page

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ class User(db.Model):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
 if __name__ == '__main__':
     app.run(debug=True)
